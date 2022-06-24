f082a0c3232a2be9246e7b44224caa2f0c6cc5f6
PamelaLimaa <layanepamela98@gmail.com>
2022-06-24 11:29:01 -0300

    service/cliente-service.js

--- Petshop/service/cliente-service.js
+++ Petshop/service/cliente-service.js
@@ -1,31 +1,10 @@
-const criaNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
-        <td class="td" data-td>${nome}</td>
-                    <td>${email}</td>
-                    <td>
-                        <ul class="tabela__botoes-controle">
-                            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                        </ul>
-                    </td> 
-                    `
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
-}
-
-
-const tabela = document.querySelector('[data-tabela]')
-
-const listaClientes = () => {
+export const listaClientes = () => {
     return fetch(`http://localhost:3000/profile`)
-    .then( resposta => {
+    .then(resposta => {
         return resposta.json()
     })
 }
 
-listaClientes()
-.then(data => {
-        data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-})})
+export const clienteService = {
+    listaClientes
+}
